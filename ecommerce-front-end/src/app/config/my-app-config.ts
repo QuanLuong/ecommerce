7627c51f97c8d0863e9a25a0cf1a48750e1da4eb
Quan Luong <dinhquan15797@gmail.com>
2021-06-04 00:45:01 +0700

    add login

---
+++ ecommerce-front-end/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oav6kvziddqwdnG65d6',
+        issuer: 'https://dev-38152231.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
